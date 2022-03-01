ff66b43ed0dcc182b6072e3d78004b21fcb246d6
dy-yb <ydyelin@gmail.com>
2022-03-01 22:20:40 +0900

    initial commit

---
+++ KDC-01-FamousSayingGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KDC-01-FamousSayingGenerator
+//
+//  Created by Daye on 2022/02/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
