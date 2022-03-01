6fe8b59dd25e69d7945954753ea2359fcd9ef498
dy-yb <ydyelin@gmail.com>
2022-03-01 22:37:23 +0900

    feat: set UIs on storyboard

--- KDC-01-FamousSayingGenerator/ViewController.swift
+++ KDC-01-FamousSayingGenerator/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var quoteLabel: UILabel!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func tapQuoteGeneratorButton(_ sender: Any) {
+  }
+  
 }
 
