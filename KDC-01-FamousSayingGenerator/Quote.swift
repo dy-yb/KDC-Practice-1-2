1694e2309b8469b7397959dff453b742cf267e04
dy-yb <ydyelin@gmail.com>
2022-03-01 22:40:23 +0900

    feat: add data and struct

---
+++ KDC-01-FamousSayingGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  KDC-01-FamousSayingGenerator
+//
+//  Created by Daye on 2022/03/01.
+//
+
+import Foundation
+
+struct Quote {
+  let contents: String
+  let name: String
+}
